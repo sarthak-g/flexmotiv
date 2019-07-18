3bf76289767c18bd1d2b29fa4eb0c8e706d14da1
sarthak-g <sarthakgupta041@gmail.com>
2019-07-18 13:12:49 +0530

    update in how login works

--- flexmotivTechnologies/settings.py
+++ flexmotivTechnologies/settings.py
@@ -129,5 +129,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'erpAPP/media')
 
-LOGIN_REDIRECT_URL = 'access'
+LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
--- flexmotivTechnologies/views.py
+++ flexmotivTechnologies/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    if request.user.is_authenticated:
+        authenticated = 'Yes'
+        return render(request,"index.html",{'authenticated':authenticated})
+    else:
+        return redirect("/login/")
+    return render(request,"index.html")
 @login_required
 def access(request):
     return render(request, "access.html")
